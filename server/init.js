54d1661c4bfb13d1e447e734e4be91a974254dd5
sangeethkumar <sangeeth@springbuck.tech>
2017-01-23 14:37:42 +0530

    sign up button alignment changes ..

---
+++ server/init.js
@@ -0,0 +1,6 @@
+
+
+Meteor.startup(function () {
+ process.env.MAIL_URL = 'smtp://postmaster@sandboxa533996dfbeb40bfb90410d04b784e5e.mailgun.org:43911f8e400a897bdaf87ee47394d266@smtp.mailgun.org:587';
+});
+
