b2a61a06f3b937f28843ce48455010d5b7d51d43
sangeethkumar <sangeeth@springbuck.tech>
2017-01-24 10:34:39 +0530

    updating bg image new

--- server/init.js
+++ server/init.js
@@ -1,6 +1,6 @@
 
 
 Meteor.startup(function () {
- process.env.MAIL_URL = 'smtp://postmaster@sandboxa533996dfbeb40bfb90410d04b784e5e.mailgun.org:43911f8e400a897bdaf87ee47394d266@smtp.mailgun.org:587';
+ process.env.MAIL_URL = 'smtp://postmaster@wequ.co:3055595afb57b03693562aceb84ad359@smtp.mailgun.org:587';
 });
 
