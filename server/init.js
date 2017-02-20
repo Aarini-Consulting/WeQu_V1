b5ab9d5865615016959e92762b6ebc865efeee64
sangeethkumar <sangeeth@springbuck.tech>
2017-02-20 12:34:40 +0530

    1001 button font color white , margin width top reduce ..

--- server/init.js
+++ server/init.js
@@ -1,10 +1,6 @@
 
 Meteor.startup(function () {
 
-   // SMTP Configuration
-  // process.env.MAIL_URL = 'smtp://postmaster@wequ.co:3055595afb57b03693562aceb84ad359@smtp.mailgun.org:587';
-  // SMTP Configuration
-   process.env.MAIL_URL = 'smtp://contact@wequ.co:Feedback12@smtp.gmail.com:465';
    // Linked in configuration
    var clientId,secret;
     if(Meteor.isDevelopment)
---
+++ server/smtp.js
@@ -0,0 +1,8 @@
+
+Meteor.startup = () => {
+
+     // SMTP Configuration
+  	 // process.env.MAIL_URL = 'smtp://postmaster@wequ.co:3055595afb57b03693562aceb84ad359@smtp.mailgun.org:587';
+  	 process.env.MAIL_URL = 'smtp://contact@wequ.co:Feedback12@smtp.gmail.com:465';
+
+}
