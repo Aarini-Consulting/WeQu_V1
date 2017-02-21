cd117666a355001919c5d2219264eb5c73379fcc
sangeethkumar <sangeeth@springbuck.tech>
2017-02-21 17:34:47 +0530

    1013 radar graph how other see me fix

--- client/templates/pages/profile/profile.js
+++ client/templates/pages/profile/profile.js
@@ -4,25 +4,3 @@
             Router.go('/profile');
         }
   });
-
-  // Due to Reactivity issues , the graph is not loading properly 
-
-  // Business logic should not be in routes.js 
-
-  // Moving it into helpers 
-
-  Template.profile.helpers({
-
-  	data(){
-  		var myfeedback = Feedback.findOne({ 'from': Meteor.userId(), 'to' : Meteor.userId(), done: true});
-  		if(myfeedback){
-  		var data = calculateTopWeak([myfeedback]);
-        data.myscore = calculateScore(myfeedback.qset);
-        data.profile = Meteor.user().profile;
-        console.log(data);
-        return data;
-        }
-        return null;
-    }
-
-  })
--- client/templates/pages/radar/radar.js
+++ client/templates/pages/radar/radar.js
@@ -5,12 +5,6 @@ Template.radar.onCreated(function(){
 	    self.subscribe("feedback");
 	  });
 
-	  // For some reasons score is getting re-initialised 
-
-	  //TODO : Check why this is happening ?
-
-	  console.log(this.data.score);
-
 	   if(this.data.score){
        	this.data.points = dataForRadar(this.data.score);
        }
