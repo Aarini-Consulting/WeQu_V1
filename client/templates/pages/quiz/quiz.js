ac832add5508215a981ab2ae010fb5c45fa7263e
sangeethkumar <sangeeth@springbuck.tech>
2017-03-09 16:08:21 +0530

    implementing the navigation logic by returning a callback from server , once the question set is generated .

--- client/templates/pages/quiz/quiz.js
+++ client/templates/pages/quiz/quiz.js
@@ -1,12 +1,12 @@
   questionDep =  new Tracker.Dependency();
-    var currentQuestion = function currentQuestion(questions) {
-        questionDep.depend();
-        return _.find(questions, function (question) {
+  var currentQuestion = function currentQuestion(questions) {
+    questionDep.depend();
+    return _.find(questions, function (question) {
             return !_.has(question, 'answer');// || !_.has(question, 'written');
         });
-    };
+};
 
- Template.quiz.helpers({
+Template.quiz.helpers({
     'writtenFeedback' : function () {
         var question = currentQuestion(this.feedback.qset);
         !(this.feedback.to == this.feedback.from) && question && _.has(question, 'answer') && !_.has(question, 'written');
@@ -34,8 +34,8 @@
 });
 
 
- var answering = false;
- Template['quiz'].events({
+var answering = false;
+Template['quiz'].events({
     "click .answer, click .skip, click .writeAnswer" : function (event, template) {
 
         event.preventDefault();
@@ -62,42 +62,6 @@
                     Meteor.users.update({_id: Meteor.userId()},
                       {$set : { "profile.gender": event.target.getAttribute('id') }});
                 }
-
-                // After reaching the last question of the set 
-                // Display the next person , instead of starting question set 4/4
-
-                var idx = 0;
-                _.find(template.data.feedback.qset, function (question) {
-                    idx++;
-                    return !_.has(question, 'answer');
-                });
-                console.log("idx = ", idx);
-
-                // Total number of questions ..
-                let questionsTotal = template.data.feedback.qset.length; 
-
-                if(idx == questionsTotal+1)
-                {
-                    console.log(template.data.nextPerson);
-                    // Navigate to the next person ..
-                    if(template.data.nextPerson == true){
-                        var friends = template.data.friends;
-                        var idx = friends.indexOf(quizPerson.get())
-                        if(idx >= 0 && idx < friends.length - 1){
-                            quizPerson.set(friends[idx + 1]);
-                        }
-                    }
-                    // Navigate to the first person (current user) ..
-                    else{
-                        var friends = template.data.friends;
-                        var idx = friends.indexOf(quizPerson.get())
-                        if(idx >= 1 && idx < friends.length){
-                           // quizPerson.set(friends[idx - 1]);
-                           quizPerson.set(friends[0]);
-                        }
-                    }
-                }
-
             }
 
             if(buttonType == 'skip'){
@@ -107,22 +71,41 @@
                     question.answer = false
                 }
             } 
-            
+
             if(buttonType == 'writeAnswer') {
                 question.written = template.$('textarea').val();
             }
 
             Meteor.call('feedback', feedback._id, feedback.qset, function (err, result) {
-                if(err) {
+                if(err){
                     console.log('feedback error', err);
                 }
-                if(result){
-                    console.log('feedback result',result);
+
+            // After reaching the last question of the set 
+            // Display the next person , instead of starting question set 4/4
+            
+            if(result)
+            {
+               if(template.data.nextPerson == true){
+                var friends = template.data.friends;
+                var idx = friends.indexOf(quizPerson.get())
+                if(idx >= 0 && idx < friends.length - 1){
+                    quizPerson.set(friends[idx + 1]);
                 }
-                answering = false;
-                questionDep.changed()
-                if(!currentQuestion(feedback.qset)) {
-                    if(Session.get('invite')){
+            }
+            else{
+                var friends = template.data.friends;
+                var idx = friends.indexOf(quizPerson.get())
+                if(idx >= 1 && idx < friends.length){
+                           // quizPerson.set(friends[idx - 1]);
+                           quizPerson.set(friends[0]);
+                       }
+                   }
+               }
+               answering = false;
+               questionDep.changed()
+               if(!currentQuestion(feedback.qset)) {
+                if(Session.get('invite')){
                         //Session.setPersistent('invite', 'filldata');
                         Session.set('invite', 'filldata');
                     } else if(getLoginScript()) {
