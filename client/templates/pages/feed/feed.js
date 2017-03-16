5f0653e2d2955e86da3259024729fafeb0e8f1a8
sangeethkumar <sangeeth@springbuck.tech>
2017-03-16 17:37:28 +0530

    type1 stmt changes

--- client/templates/pages/feed/feed.js
+++ client/templates/pages/feed/feed.js
@@ -1,17 +1,16 @@
 Template.feed.helpers({
 	feeds(){
 
-		 return Feeds.find({}, {sort: {createdAt: -1}  }, {
+		 return  Feeds.find( {}, {
 	                    transform: function (doc) {
 	                        doc.userInfo = Meteor.users.findOne({_id: doc.inviteId});
-	                        doc.displayTrue = Meteor.users.findOne({_id: doc.inviteId})&& 
-	                        					Meteor.users.findOne({_id: doc.inviteId})._id == Meteor.userId();
-	                        doc.filter = Meteor.users.findOne({_id: doc.id}) === Meteor.userId();
-	                        console.log(doc);
+	                        doc.displayTrue = Meteor.users.findOne({_id: doc.inviteId})&& Meteor.users.findOne({_id: doc.inviteId})._id == Meteor.userId();
+	                        doc.filter =  Meteor.users.findOne({_id: doc.id}) && Meteor.users.findOne({_id: doc.id})._id == Meteor.userId() ? true : false ;
+	                       // console.log(doc);
 	                        return doc;
 	                    }      
-                   }); 
-	     }    
+                   } ).fetch();  // For some reason sort is not working  {sort: {createdAt: -1} }
+		}    
 
 });
 
