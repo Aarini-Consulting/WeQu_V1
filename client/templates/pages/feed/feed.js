3d755466c9fbfdd2d117576e70a5779bc6e34150
sangeethkumar <sangeeth@springbuck.tech>
2017-03-17 11:16:04 +0530

    feeds sorting using alternative way ..

--- client/templates/pages/feed/feed.js
+++ client/templates/pages/feed/feed.js
@@ -1,15 +1,35 @@
 Template.feed.helpers({
 	feeds(){
 
-		 return  Feeds.find( {}, {
+		 // For some reason sort is not working  
+		/* let data = Feeds.find( {}, {
 	                    transform: function (doc) {
 	                        doc.userInfo = Meteor.users.findOne({_id: doc.inviteId});
 	                        doc.displayTrue = Meteor.users.findOne({_id: doc.inviteId})&& Meteor.users.findOne({_id: doc.inviteId})._id == Meteor.userId();
-	                        doc.filter =  Meteor.users.findOne({_id: doc.id}) && Meteor.users.findOne({_id: doc.id})._id == Meteor.userId() ? true : false ;
-	                       // console.log(doc);
+	                        doc.filter = false;
+	                        if(Meteor.users.findOne({_id: doc.id})){
+							  doc.filter =  Meteor.users.findOne({_id: doc.id})._id == Meteor.userId() ? true : false ;
+	                        } 
 	                        return doc;
 	                    }      
-                   } ).fetch();  // For some reason sort is not working  {sort: {createdAt: -1} }
+                   } ,{sort: {createdAt: 1}} ); */
+         
+         // TODO :  USE the above logic .
+
+         data = Feeds.find({},{sort: {createdAt: -1} });
+
+ 	     let data2 = [];
+ 	     let i=0;
+		 data.forEach(function (doc) {
+		 	 doc.userInfo = Meteor.users.findOne({_id: doc.inviteId});
+             doc.displayTrue = Meteor.users.findOne({_id: doc.inviteId})&& Meteor.users.findOne({_id: doc.inviteId})._id == Meteor.userId();
+             doc.filter =  Meteor.users.findOne({_id: doc.id}) && Meteor.users.findOne({_id: doc.id})._id == Meteor.userId() ? true : false ;
+             data2[i] = doc;
+             i++;
+		 });
+
+		 return data2;
+
 		}    
 
 });
