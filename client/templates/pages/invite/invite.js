4a75c41c9a493de26043c0c6cb3f8e170745a272
sangeethkumar <sangeeth@springbuck.tech>
2017-05-05 11:58:48 +0530

    adding UI validation invite friends (normal user) page

--- client/templates/pages/invite/invite.js
+++ client/templates/pages/invite/invite.js
@@ -3,10 +3,8 @@ inviteStatus = new ReactiveVar('default');
 step = new ReactiveVar('default');
 
 Template.invite.created = function () {
-        this.gender = new ReactiveVar('Male'); // Setting default to male , since in UI default value is male
         normalView = true;
         normalView = new ReactiveVar(normalView);
-
 }
 
     Template.invite.helpers({
@@ -48,91 +46,11 @@ Template.invite.created = function () {
     })
 
 
+Template.invite.events({
 
-    Template.invite.events({
-
-    "click #sendInvite" : function (event, template) {
-        console.log("event");
+    "click .step-invitebttn , click .w-inline-block" : function(event,template){
         event.preventDefault();
-        inviteStatus.set('sending');
-        var email = template.$('input[name=email]').val();
-        var name = template.$('input[name=name]').val();
-
-        var gender = template.gender.get(); //template.find('#gender').value;
-
-        // Sending invite email to self , avoided .
-
-        if(email == ( Meteor.user() && Meteor.user().emails && Meteor.user().emails[0].address ) || 
-            email == ( Meteor.user() && Meteor.user().profile.emailAddress ) ){
-                inviteStatus.set('error');
-                setInterval(function () {
-                    return inviteStatus.set('default');
-                }, 3000);
-        }
-
-        else
-        {
-             let oldUser = Connections.findOne( { $and : [   {"email":email},{"inviteId": Meteor.userId()} ] } );
-             let exists = !oldUser ? false : true;
-             console.log(exists);
-
-             if(!exists){
-                Meteor.call('invite', name, email, gender, function (err, userId) {
-                    if(err){
-                        console.log("error", err);
-                        inviteStatus.set('error');
-                        return;
-                    }
-                    template.$('input[name=name]').val('')
-                    template.$('input[name=email]').val('');
-                    inviteStatus.set('sent');
-
-                    setInterval(function () {
-                        step.set('default');
-                        return inviteStatus.set('default');
-                    }, 3000);
-                        quizPerson.set(userId);
-                        return setLoginScript('finish');
-                        console.log(err, userId);
-                });
-            }
-            else{
-                inviteStatus.set('alreadyInvited');
-                setInterval(function () {
-                        return inviteStatus.set('default');
-                }, 3000);
-            }
-        }
-
-    },
-    "click #next" : function () {
-        return setLoginScript('finish');
-    },
-    "click #male" : function(event,template){
-        event.preventDefault();
-        template.gender.set('Male');
-    },
-
-    "click #female" : function(event,template){
-        event.preventDefault();
-        template.gender.set('Female');
-    },
-
-    "click .invitebttn , click .w-inline-block" : function(event,template){
-        event.preventDefault();
-        step.set('invitebttn');
+        step.set('invitebttn'); 
     }
-    
-})
 
-
-    Template.invite.rendered = function(){
-
-        $('.gender').on('click', function(){
-            if(!$(this).hasClass('selected'))
-            {
-                $('.gender').toggleClass('selected');
-            }
-        });
-
-    }
+});
