d17d2824b8e3f571991c3b9b3e25bed9b4699baa
sangeethkumar <sangeeth@springbuck.tech>
2017-05-17 12:24:22 +0530

    added 2.How many questions other invitees answered for me .

--- client/templates/pages/profile/profileQuestionInfo/profileQuestionInfo.js
+++ client/templates/pages/profile/profileQuestionInfo/profileQuestionInfo.js
@@ -6,13 +6,15 @@ Template.profileQuestionInfo.helpers({
       let count = Feedback.find({from: Meteor.userId(), to: Meteor.userId(),done:true}).count();
       count = count*12;
       var a = Feedback.findOne({from: Meteor.userId(), to: Meteor.userId(), done:false});
-      var idx = -1;
+      var idx = 0;
       if(a){
       _.find(a.qset, function (question) {
         idx++;
         return !_.has(question, 'answer');
        });
+       idx--;
       }
+      
       idx = idx+count;
       return idx;
     },
@@ -20,14 +22,14 @@ Template.profileQuestionInfo.helpers({
     questionInviteesAnswered(){
       let count = Feedback.find({to: Meteor.userId(),done:true, from: { '$ne': Meteor.userId() }}).count();
       count = count*12;
-      var a = Feedback.findOne({to: Meteor.userId(), done:false });
-      var idx = -1;
+      var a = Feedback.findOne({to: Meteor.userId(), done:false, from: { '$ne': Meteor.userId() } });
+      var idx = 0;
       if(a){
-      count2 = _.find(a.qset, function (question) {
+       _.find(a.qset, function (question) {
         idx++;
         return !_.has(question, 'answer');
        });
-      console.log(count2);
+      idx--;
       }
       idx = idx+count;
       return idx;
