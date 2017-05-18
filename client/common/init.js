20d1754e06b06c9fdecc6320eacc4ad7697ccb00
Ritushree <Ritushree@aarini.co>
2017-05-18 15:27:36 +0200

    included google webfont in app
    included google webfont in app

--- client/common/init.js
+++ client/common/init.js
@@ -1,4 +1,18 @@
 Meteor.startup(function() {
+        WebFontConfig = {
+          google: { families: ["Raleway:100,200,300,regular,500,600,700,800,900"]}
+        };
+        (function() {
+          var wf = document.createElement('script');
+          wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+          wf.type = 'text/javascript';
+          wf.async = 'true';
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(wf, s);
+          console.log("async fonts loaded", WebFontConfig);
+        })();   
+
 
       sAlert.config({
         effect: 'genie',
