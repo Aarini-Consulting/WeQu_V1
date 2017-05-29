dfffb6b1bb6b0c9d962862b51eadbe5cf49afd08
gsangeethkumar <sangeeth.kongu@gmail.com>
2017-05-29 17:12:57 +0530

    skills to respong individually ..

--- client/templates/pages/skills/threeSkills/threeSkills.js
+++ client/templates/pages/skills/threeSkills/threeSkills.js
@@ -12,12 +12,23 @@ Template.threeSkills.created = function () {
 
 
 Template.threeSkills.events({
-	'click .swapDescription': function (event,template) {
+	'click .swapDescription1': function (event,template) {
 		event.preventDefault();
-		let isTrue = template.swapDescription.get();
-		if(isTrue)
-			template.swapDescription.set(false);
-		else
-			template.swapDescription.set(true);
+        
+        $(event.currentTarget).toggleClass('active');
+        let skill = $(event.currentTarget).attr('data-skill');
+        let c = ".swap2"+skill;
+        console.log(c);
+        $(c).toggleClass('active');
+			
+	},
+	'click .swapDescription2': function (event,template) {
+		event.preventDefault();
+        
+        $(event.currentTarget).toggleClass('active');
+        let skill = $(event.currentTarget).attr('data-skill');
+        let c = ".swap1"+skill;
+        $(c).toggleClass('active');
+			
 	}
 });
