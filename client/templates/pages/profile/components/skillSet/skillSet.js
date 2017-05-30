634bb9b3c742b2447bd8532b35c0a93fb1d2fe55
gsangeethkumar <sangeeth.kongu@gmail.com>
2017-05-30 10:05:05 +0530

    adding logic for is scetion empty ..

---
+++ client/templates/pages/profile/components/skillSet/skillSet.js
@@ -0,0 +1,25 @@
+Template.skillSet.helpers({
+
+	sectionEmpty(){
+		let count= Connections.find( { $or : [ {inviteId:Meteor.userId()} ,
+                                      {email : Meteor.user().emails && Meteor.user().emails[0].address},
+                                      {email : Meteor.user().profile && Meteor.user().profile.emailAddress}   ] }                                                       
+                                     ).count();
+        let countAlpha="", i= 3;
+        let count2= i-count;
+        if(count2==1){
+        	countAlpha = "ONE"
+        }
+        if(count2==2){
+        	countAlpha = "TWO"
+        }
+        if(count2==3){
+        	countAlpha = "THREE"
+        }
+
+		return {
+			isTrue: count <2,
+			count : countAlpha
+		}
+	}
+});
