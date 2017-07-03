b58e97e68eab4c577b82aa66d5659aa96c55bdbc
sangeethkumar <sangeeth@springbuck.tech>
2017-07-03 16:20:56 +0530

    unlock the skill - if invited as a group member also

--- client/templates/pages/profile/components/skillSet/skillSet.js
+++ client/templates/pages/profile/components/skillSet/skillSet.js
@@ -1,3 +1,10 @@
+Template.skillSet.onCreated(function() {
+  var self = this;
+  self.autorun(function() {
+    self.subscribe("group");
+  });
+});
+
 Template.skillSet.helpers({
 
 	sectionEmpty(){
@@ -5,17 +12,20 @@ Template.skillSet.helpers({
                                       {email : Meteor.user().emails && Meteor.user().emails[0].address},
                                       {email : Meteor.user().profile && Meteor.user().profile.emailAddress}   ] }                                                       
                                      ).count();
-        let countAlpha="", i= 3;
-        let count2= i-count;
-        if(count2==1){
-        	countAlpha = "ONE"
-        }
-        if(count2==2){
-        	countAlpha = "TWO"
-        }
-        if(count2==3){
-        	countAlpha = "THREE"
-        }
+    let email = ( Meteor.user() && Meteor.user().emails && Meteor.user().emails[0].address ) || ( Meteor.user()  && Meteor.user().profile.emailAddress) ;
+    let count3 = Group.find({"arr_emails_existing":email}).count();
+
+    let countAlpha="", i= 3;
+    let count2= i-count-count3 ;
+    if(count2==1){
+    	countAlpha = "ONE"
+    }
+    if(count2==2){
+    	countAlpha = "TWO"
+    }
+    if(count2==3){
+    	countAlpha = "THREE"
+    }
     let bool = quizPerson.get() == Meteor.userId()  && count <3;
 		return {
 			isTrue: bool,
