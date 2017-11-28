e02ea93e43264df2e92d2946c491795bcc29dede
yohandi-w-aarini <Yohandi@aarini.co>
2017-11-28 18:00:48 +0100

    added scroll effect on profile page
    -added scroll effect that anchor sectionProgress on the top of the page when certain level of scroll down is reached

--- imports/ui/pages/profile/Profile.jsx
+++ imports/ui/pages/profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Link, Redirect } from 'react-router';
@@ -14,10 +15,49 @@ import SectionProfile from './SectionProfile';
 import '/imports/startup/client/wequ-profile.webflow.css';
 
 class Profile extends React.Component {
+  handleScroll(ev) {
+    var scroll = ev && ev.target && ev.target.scrollTop;
+    var progress = ev.target.children[1];
+    if(scroll && progress){
+      if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){
+        if (scroll >= 300) {
+          progress.classList.add('fix-searchIOS');
+        } else {
+          progress.classList.remove("fix-searchIOS");
+        }
+       }else{
+          if (scroll >= 300) {
+            progress.classList.add('fix-search');
+          } else {
+            progress.classList.remove("fix-search");
+          }
+       }
+    }
+  }
+  componentWillUnmount() {
+      const feed = ReactDOM.findDOMNode(this.refs.feed);
+      feed.removeEventListener('scroll', this.handleScroll);
+  }
+
+  componentDidMount(prevProps, prevState){
+    const feed = ReactDOM.findDOMNode(this.refs.feed);
+    if(feed){
+      feed.addEventListener('scroll', this.handleScroll);
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    const feed = ReactDOM.findDOMNode(this.refs.feed);
+    if(feed){
+      feed.addEventListener('scroll', this.handleScroll);
+    }
+  }
+
   render() {
+    
     if(this.props.currentUser && this.props.currentUser.profile && this.props.currentUser.profile.loginScript){
       return (
-      <section className="feed" id="feed">
+      <section className="feed" id="feed" ref="feed">
 
         <div className="sectionname">
           <div className="profilename w-container">
@@ -37,7 +77,7 @@ class Profile extends React.Component {
           </div>
         </div>
 
-        <SectionProgress quizPerson={Meteor.userId()}/>
+        <SectionProgress quizPerson={Meteor.userId()} ref="sectionProgress"/>
 
         <div className="progressdesdiv">
           <div className="fontprogressdes fontreleway small">Below&nbsp;information is being generated based on the inputs from you and your teammates</div>
