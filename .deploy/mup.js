98bb35463cf5dd19e0e0e57791872a6721c420ef
sahaRitu <Ritushree@aarini.co>
2018-03-22 22:03:22 +0100

    update mup.json

--- .deploy/mup.js
+++ .deploy/mup.js
@@ -18,7 +18,7 @@ module.exports = {
       serverOnly: true,
     },
     env: {
-      ROOT_URL: 'http://app.wequ.co',
+      ROOT_URL: 'http://app.weq.io',
       MAIL_URL: 'smtp://postmaster@wequ.co:Feedback12@smtp.office365.com:587',
 	  MONGO_URL: 'mongodb://WeQuAdmin:JZR63MSg4KenTW38@wequ-feedback-app-shard-00-00-tdmtm.mongodb.net:27017/wequ?ssl=true&replicaSet=WeQu-Feedback-App-shard-0&authSource=admin'
 	},
