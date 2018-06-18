a0d0bdc68e6ebabdc8500a1151c3d7574806993a
Ritushree <ritushree@aarini.co>
2018-06-18 16:52:09 +0200

    Updated mup to new mailurl

--- .deploy/mup.js
+++ .deploy/mup.js
@@ -3,14 +3,14 @@ module.exports = {
     one: {
       host: '52.29.222.185',
       username: 'ubuntu',
-      pem: 'C:/\Users/\Ritushree/\Documents/\GitHub/\WeQu_V1/\WeQu.pem',
+      pem: '/Users/aarini/Documents/GitHub/WeQu_V1/WeQu.pem',
       // password:
       // or leave blank for authenticate from ssh-agent
     }
   },
   meteor: {
     name: 'wequ',
-    path: 'C:/\Users/\Ritushree/\Documents/\GitHub/\WeQu_V1',
+    path: '/Users/aarini/Documents/GitHub/\WeQu_V1',
     servers: {
       one: {}
     },
@@ -19,7 +19,7 @@ module.exports = {
     },
     env: {
       ROOT_URL: 'http://app.weq.io',
-      MAIL_URL: 'smtp://postmaster@wequ.co:Feedback12@smtp.office365.com:587',
+      MAIL_URL: 'smtp://postmaster@weq.io:Feedback12@smtp.office365.com:587',
 	  MONGO_URL: 'mongodb://WeQuAdmin:JZR63MSg4KenTW38@wequ-feedback-app-shard-00-00-tdmtm.mongodb.net:27017/wequ?ssl=true&replicaSet=WeQu-Feedback-App-shard-0&authSource=admin'
 	},
 
