5123cf0fa04c04a30d27e7161084b3b08fad30a9
yohandi-w-aarini <Yohandi@aarini.co>
2018-10-10 17:52:10 +0200

    Added function to send twillio sms invites + updated user msisdn data structure

--- server/wetime.js
+++ server/wetime.js
@@ -1,46 +1,95 @@
 Meteor.methods({
-    'update.msisdn' : function (msisdn) {
+    'update.selected.group' : function (groupId) {
         if(this.userId){
             Meteor.users.update({_id: this.userId},
                 {$set: {
-                    'msisdn' : msisdn,
+                    'lastSelectedGroupId' : groupId,
+                } 
+            });
+        }
+    },
+    'update.mobile' : function (countryCode, msisdn) {
+        if(this.userId){
+            Meteor.users.update({_id: this.userId},
+                {$set: {
+                    'mobile' : [{'countryCode':countryCode, 'number':msisdn, 'verified':true}],
                 } 
             });
         }
     },
     'send.verification.sms' : function (countryCode, phoneNumber) {
         if(this.userId){
-            var result = HTTP.call( 'POST', 'https://api.authy.com/protected/json/phones/verification/start', 
-            {
-                data: {
-                    api_key: 'ESDI1HGJzl9zRqIlTtQpJYNOLeOMPkWf',
-                    via: 'sms',
-                    phone_number: phoneNumber,
-                    country_code: countryCode
-                    }
-            });
+            try{
+                HTTP.call( 'POST', 'https://api.authy.com/protected/json/phones/verification/start', 
+                {
+                    data: {
+                        api_key: Meteor.settings.private.twillioVerificationApiKey,
+                        via: 'sms',
+                        phone_number: phoneNumber,
+                        country_code: countryCode
+                        }
+                });
+                return true;
+            }
+            catch(e){
+                console.log(e);
+                return false;
+            }
 
-            return result;
         }else{
             throw new Meteor.Error("user undefined");
         }
     },
     'verify.number' : function (countryCode, phoneNumber, verificationCode) {
         if(this.userId){
-            var result = HTTP.call( 'GET', 'https://api.authy.com/protected/json/phones/verification/check', 
-            {
-                params: {
-                    api_key: 'ESDI1HGJzl9zRqIlTtQpJYNOLeOMPkWf',
-                    verification_code: verificationCode,
-                    phone_number: phoneNumber,
-                    country_code: countryCode
-                    }
-            });
-            if(result && result.data && result.data.success){
-                Meteor.call('update.msisdn',phoneNumber);
+            try{
+                var result = HTTP.call( 'GET', 'https://api.authy.com/protected/json/phones/verification/check', 
+                {
+                    params: {
+                        api_key: Meteor.settings.private.twillioVerificationApiKey,
+                        verification_code: verificationCode,
+                        phone_number: phoneNumber,
+                        country_code: countryCode
+                        }
+                });
+                if(result && result.data && result.data.success){
+                    Meteor.call('update.mobile',countryCode, phoneNumber);
+                }
+                return true;
+            }
+            catch(e){
+                console.log(e);
+                return false;
             }
 
-            return result;
+        }else{
+            throw new Meteor.Error("user undefined");
+        }
+    },
+    'send.sms.invitation' : function (msisdn, link) {
+        if(this.userId){
+            check(link, String);
+            if(link.startsWith('https://wetime.page.link/')){
+                var message=`Oh boy, looks like somebody wants you to join WeTime:${link}`
+                try{
+                    HTTP.call( 'POST', `https://api.twilio.com/2010-04-01/Accounts/${Meteor.settings.private.twillioNotificationAccountSid}/Messages.json`, 
+                    {
+                        params: {
+                            To: msisdn,
+                            From: Meteor.settings.private.twillioNotificationSenderMsisdn,
+                            Body: message,
+                        },
+                        auth:`${Meteor.settings.private.twillioNotificationAccountSid}:${Meteor.settings.private.twillioNotificationAuthKey}`
+                    });
+                    return true;
+                }
+                catch(e){
+                    console.log(e);
+                    return false;
+                }
+            }else{
+                throw new Meteor.Error("malformed link");
+            }
         }else{
             throw new Meteor.Error("user undefined");
         }
