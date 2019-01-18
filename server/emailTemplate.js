1c3a6fcf6ae78c22cf72c98afda785d19c7e1220
yohandi-w-aarini <Yohandi@aarini.co>
2019-01-18 16:42:07 +0100

    WiP translation work
    -removed unused translation lines
    -added placeholder html email template files for NL and FR

--- server/emailTemplate.js
+++ server/emailTemplate.js
@@ -6,8 +6,8 @@ Template.htmlEmail.helpers({
 
 SSR.compileTemplate('GroupInviteHtmlEmail', Assets.getText('group-invite-email.html'));
 SSR.compileTemplate('GroupInviteHtmlEmail-en', Assets.getText('group-invite-email.html'));
-SSR.compileTemplate('GroupInviteHtmlEmail-nl', Assets.getText('group-invite-email.html'));
-SSR.compileTemplate('GroupInviteHtmlEmail-fr', Assets.getText('group-invite-email.html'));
+SSR.compileTemplate('GroupInviteHtmlEmail-nl', Assets.getText('group-invite-email-nl.html'));
+SSR.compileTemplate('GroupInviteHtmlEmail-fr', Assets.getText('group-invite-email-fr.html'));
 
 Template.GroupInviteHtmlEmail.helpers({
     //Add any helper methods to enable default data in template
