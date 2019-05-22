ded50d9b82addaa666b9ed5fd04be36550f31f1e
yohandi-w-aarini <Yohandi@aarini.co>
2019-05-22 13:30:06 +0200

    WiP WA-107
    -changed condition when stopping "play card" game. keep all data and continue where they left off when start again

--- imports/ui/pages/group/GroupPlayCardPage.jsx
+++ imports/ui/pages/group/GroupPlayCardPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 
+import {PlayCard} from '/collections/playCard';
+
 import SweetAlert from '/imports/ui/pages/sweetAlert/SweetAlert';
 import WelcomePage from '/imports/ui/pages/groupPlayCard/WelcomePage';
 import GameplayPage from '/imports/ui/pages/groupPlayCard/GameplayPage';
+
+import Loading from '/imports/ui/pages/loading/Loading';
 
 class GroupPlayCardPage extends React.Component {
     constructor(props){
@@ -29,35 +33,45 @@ class GroupPlayCardPage extends React.Component {
 
     render() {
         var selectedCardType = this.props.selectedCardType;
-
-        if(this.props.group.isPlaceCardFinished){
-            if(selectedCardType){
-                if(this.props.selectedCardTypeActive){
-                    return (
-                        <div className="tap-content-wrapper play-card">
-                            <GameplayPage group={this.props.group} groupType={selectedCardType}/>
-                        </div>
-                    );
+        if(this.props.dataReady){
+            if(this.props.group.isPlaceCardFinished){
+                if(selectedCardType){
+                    if(this.props.selectedCardTypeActive){
+                        return (
+                            <div className="tap-content-wrapper play-card">
+                                <GameplayPage group={this.props.group} groupType={selectedCardType}/>
+                            </div>
+                        );
+                    }else{
+                        return (
+                            <div className="tap-content-wrapper play-card">
+                                <WelcomePage groupType={selectedCardType} confirmStartGame={this.confirmStartGame.bind(this,selectedCardType)}/>
+                
+                                {this.state.showConfirmStart &&
+                                    <SweetAlert
+                                    type={"confirm"}
+                                    message={"Everyone ready for interactive mode?"}
+                                    imageUrl={"/img/gameMaster/interactive.gif"}
+                                    confirmText={"Let's go!"}
+                                    cancelText={"Cancel"}
+                                    onCancel={() => {
+                                        this.setState({ showConfirmStart: false });
+                                    }}
+                                    onConfirm={() => {
+                                        this.startGamePlayCards(selectedCardType);
+                                        this.setState({ showConfirmStart: false });
+                                    }}/>
+                                }
+                            </div>
+                        );
+                    }
                 }else{
-                    return (
+                    return(
                         <div className="tap-content-wrapper play-card">
-                            <WelcomePage groupType={selectedCardType} confirmStartGame={this.confirmStartGame.bind(this,selectedCardType)}/>
-            
-                            {this.state.showConfirmStart &&
-                                <SweetAlert
-                                type={"confirm"}
-                                message={"Everyone ready for interactive mode?"}
-                                imageUrl={"/img/gameMaster/interactive.gif"}
-                                confirmText={"Let's go!"}
-                                cancelText={"Cancel"}
-                                onCancel={() => {
-                                    this.setState({ showConfirmStart: false });
-                                }}
-                                onConfirm={() => {
-                                    this.startGamePlayCards(selectedCardType);
-                                    this.setState({ showConfirmStart: false });
-                                }}/>
-                            }
+                            <div className="div-block-center">
+                                <div className="ring"></div>
+                                <h1>No "play card" mode is assigned to this group</h1>
+                            </div>
                         </div>
                     );
                 }
@@ -66,21 +80,17 @@ class GroupPlayCardPage extends React.Component {
                     <div className="tap-content-wrapper play-card">
                         <div className="div-block-center">
                             <div className="ring"></div>
-                            <h1>No "play card" mode is assigned to this group</h1>
+                            <h1>You need to finish "Prepare Cards" first before using this features</h1>
                         </div>
                     </div>
                 );
             }
         }else{
             return(
-                <div className="tap-content-wrapper play-card">
-                    <div className="div-block-center">
-                        <div className="ring"></div>
-                        <h1>You need to finish "Prepare Cards" first before using this features</h1>
-                    </div>
-                </div>
+                <Loading/>
             );
         }
+        
     }
 }
 
@@ -88,36 +98,53 @@ export default withTracker((props) => {
     let selectedCardType;
     let playCardFinished=false;
     let selectedCardTypeActive=false;
+    let dataReady = false;
 
     if(props.group.playCardTypeActive){
         selectedCardType = props.group.playCardTypeActive;
         selectedCardTypeActive = true;
+        dataReady = true;
     }else{
         //currently no playcard mode is selected as active
         //check if this group has playcard modes assigned to it
         if(props.group.playCardTypeList && props.group.playCardTypeList.length > 0){
-            //check if any mode is ever completed
-            if(props.group.playCardTypeCompleted && props.group.playCardTypeCompleted.length > 0){
-                //choose the last one completed
-                selectedCardType = props.group.playCardTypeCompleted[(props.group.playCardTypeCompleted.length-1)];
-                //check if all play card mode is completed
-                if(props.group.playCardTypeCompleted.length == props.group.playCardTypeList.length){
-                    //set value to indicate all mode is completed
-                    playCardFinished = true;
+            //check if any mode is ever attempted
+            let handlePlayCard = Meteor.subscribe('playCard',
+                {
+                    "groupId":props.group._id,
+                    'cardChosen':{$exists: true}
+                },{sort: { "createdAt": -1 }}, {
+                onError: function (error) {
+                    console.log(error);
                 }
-            }else{
-                //no mode is completed, select the first one
-                selectedCardType = props.group.playCardTypeList[0];
+            });
 
+            if(handlePlayCard.ready()){
+                let playCardCheck = PlayCard.findOne({
+                    "groupId":props.group._id,
+                    'cardChosen':{$exists: true}
+                },{sort: { "createdAt": -1 }});
 
-                //TODO:
-                //check if any mode is ever attempted and continue where they left it off
+                if(playCardCheck){
+                    //call start function to resume game
+                    Meteor.call( 'start.game.play.cards', props.group._id, playCardCheck.playCardType, (error, result)=>{
+                        if(error){
+                            console.log(error)
+                        }
+                    });
+                }else{
+                    //no mode is completed, select the first one
+                    selectedCardType = props.group.playCardTypeList[0];
+                    dataReady = true;
+                }
             }
+            
         }
     }
     return {
         selectedCardType:selectedCardType,
         selectedCardTypeActive:selectedCardTypeActive,
-        playCardFinished:playCardFinished
+        playCardFinished:playCardFinished,
+        dataReady:dataReady
     };
   })(GroupPlayCardPage);
